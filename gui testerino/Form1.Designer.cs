c326b313f4b6c918f26dfedcb721a0daa3daf92d
RCryeji <raphaelchase302@gmail.com>
2023-12-10 20:29:49 +0800

    changed text

--- gui testerino/Form1.Designer.cs
+++ gui testerino/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1232, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Testerino";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
